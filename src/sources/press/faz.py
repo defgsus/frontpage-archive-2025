dc05f453c90669afbf307f71a95447b6baf29306
bergi <s.berke@netzkolchose.de>
2025-09-02 21:49:46 +0200

    Fix faz.net scraper (they now use the standard <article> style) and update README

--- src/sources/press/faz.py
+++ src/sources/press/faz.py
@@ -22,36 +22,3 @@ class FrankfurterAllgemeine(Scraper):
         ("wissen", URL + "aktuell/wissen/"),
         ("reise", URL + "aktuell/reise/"),
     ]
-
-    def iter_articles(self, url: str, filename: str, content: str) -> Generator[dict, None, None]:
-        soup = self.to_soup(content)
-        for tag in soup.find_all("div", {"class": "teaser-object"}):
-            if not (tag.text and tag.text.strip()):
-                continue
-
-            headline = self.find_headline(tag)
-            headline = self.strip(headline)
-            if not headline:
-                continue
-
-            article = self.create_article_dict(
-                title=headline,
-                teaser=(
-                    self.strip(tag.find("div", {"class": "p2-teaser"}))
-                ),
-            )
-
-            a = tag.find("a")
-            if a and a.get("href"):
-                article["url"] = self.url_join(url, a["href"])
-
-            image = tag.find("img")
-            if image and image.get("src"):
-                article["image_url"] = image["src"]
-                article["image_title"] = self.strip(
-                    image.get("alt") or image.get("title")
-                )
-
-            self.patch_article(article, tag)
-
-            yield article
